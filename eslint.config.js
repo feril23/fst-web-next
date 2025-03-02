99f5b017970e6331ae5f995a2936b4d2d7326352
feril <ferilferdian4@gmail.com>
2025-03-03 04:42:49 +0700

    fix

--- eslint.config.js
+++ eslint.config.js
@@ -18,8 +18,8 @@ export default tseslint.config(
       "react-refresh": reactRefresh,
     },
     rules: {
-      "react/no-unescaped-entities": "off",
-      "@next/next/no-page-custom-font": "off",
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
     },
   }
 );
