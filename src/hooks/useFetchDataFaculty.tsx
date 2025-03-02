c59a3ca18f8f2893ef3af2866f088ab063abd1a0
feril <ferilferdian4@gmail.com>
2025-03-03 06:17:09 +0700

    coba

--- src/hooks/useFetchDataFaculty.tsx
+++ src/hooks/useFetchDataFaculty.tsx
@@ -2,11 +2,14 @@ const CACHE_EXPIRATION = 600; // 10 menit dalam DETIK (untuk ISR)
 
 export async function getFacultyData() {
   try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/faculty`, {
+    const res = await fetch(`/api/faculty`, {
       next: { revalidate: CACHE_EXPIRATION }, // ISR: revalidate setiap 10 menit
     });
 
-    if (!res.ok) throw new Error(`Gagal mengambil data:  ${res.status}`);
+    if (!res.ok) {
+      const text = await res.text();
+      throw new Error(`Gagal mengambil data:  ${res.status} Response: ${text}`);
+    }
     return res.json();
   } catch (error) {
     throw new Error("Gagal mengambil data fakultas");
