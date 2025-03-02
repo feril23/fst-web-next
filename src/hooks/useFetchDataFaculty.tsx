e8b67ddb7b0ad8e870fa9ad2ed60c5b89a5925cc
feril <ferilferdian4@gmail.com>
2025-03-03 06:31:09 +0700

    fix

--- src/hooks/useFetchDataFaculty.tsx
+++ src/hooks/useFetchDataFaculty.tsx
@@ -2,7 +2,7 @@ const CACHE_EXPIRATION = 600; // 10 menit dalam DETIK (untuk ISR)
 
 export async function getFacultyData() {
   try {
-    const res = await fetch(`/api/faculty`, {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/faculty`, {
       next: { revalidate: CACHE_EXPIRATION }, // ISR: revalidate setiap 10 menit
     });
 
