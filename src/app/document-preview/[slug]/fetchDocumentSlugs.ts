d3fd9f51288f615951d1cf659275d92f6b2d5f01
feril <ferilferdian4@gmail.com>
2025-03-03 22:16:27 +0700

    fix

--- src/app/document-preview/[slug]/fetchDocumentSlugs.ts
+++
@@ -1,8 +0,0 @@
-import { useDocument } from "../../../contexts/DocumentContext";
-
-const FethDocumentSlug = () => {
-  const { documentName } = useDocument();
-  return documentName;
-};
-
-export default FethDocumentSlug;
--- src/app/document-preview/[slug]/page.tsx
+++ src/app/document-preview/[slug]/page.tsx
@@ -1,18 +1,11 @@
 import { Metadata } from "next";
 import DocumentPreview from "./DocumentPreview";
-import FethDocumentSlug from "./fetchDocumentSlugs";
-
-export async function generateStaticParams() {
-  const name = FethDocumentSlug(); // Fungsi untuk get semua slug
-
-  return name;
-}
 
 export const dynamicParams = true;
 
-export async function generateMetadata({ params }: { params: Promise<{ slug: string }> }): Promise<Metadata> {
+export async function generateMetadata({ params }: { params: { slug: string } }): Promise<Metadata> {
   // Convert slug to title case (e.g., "teknik-informatika" -> "Teknik Informatika")
-  const title = (await params).slug;
+  const title = params.slug;
 
   return {
     title: `${title} | Fakultas Sains dan Teknologi UIN Ar-Raniry`,
